6f831a7097ba31ca6a04f469dc44e71b3e00ec5e
CyBoul <cyrilboulanger87@gmail.com>
2026-04-04 11:43:30 +0200

    FEAT PaymentService: Kafka event consumer

---
+++ payment-service/src/main/java/com/cyboul/cloudplatform/paymentservice/OrderCreatedEventConsumer.java
@@ -0,0 +1,22 @@
+package com.cyboul.cloudplatform.paymentservice;
+
+import com.cyboul.cloudplatform.common.events.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderCreatedEventConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderCreatedEventConsumer.class);
+
+    @KafkaListener(topics = "order-created", groupId = "${spring.kafka.consumer.group-id}")
+    public void onOrderCreated(OrderCreatedEvent event) {
+        log.info("Received OrderCreatedEvent: orderId={}, userId={}, productId={}, amount={}",
+                event.orderId(), event.userId(), event.productId(), event.amount());
+
+        // process payment
+        System.out.println("Received OrderCreatedEvent: orderId=" + event.orderId());
+    }
+}
